2305fa6ad45bbc98c77310a75f915998c206fc4e
kyle <kyle@astorial.io>
2023-02-02 20:14:21 +0000

    Updated library function

--- rust/2022/aoc_lib/src/lib.rs
+++ rust/2022/aoc_lib/src/lib.rs
@@ -1,20 +1,5 @@
 use std::fs;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
-
 pub fn get_input_as_string() -> String {
 
     match fs::read_to_string("input.txt") {
