216196ce7c1a52bc84cf1fd69ca8a29f9b9f5b8d
kyle <kyle@astorial.io>
2023-02-03 15:57:16 +0000

    2022 day 4 pt 2 (rust)

--- rust/2022/day-4/src/main.rs
+++ rust/2022/day-4/src/main.rs
@@ -15,6 +15,7 @@ fn main() {
     //println!("Pairs: {:?}", pairs);
 
     let mut full_overlaps: u32 = 0;
+    let mut partial_overlaps: u32 = 0;
 
     for pair in pairs {
         let (l, r) = pair;
@@ -27,11 +28,20 @@ fn main() {
         else if l_low <= r_low && l_high >= r_high {    // Right contained within Left
             full_overlaps += 1;
         }
+        else if l_low <= r_low && l_high >= r_low {
+            partial_overlaps += 1;
+        }
+        else if l_low <= r_high && l_high >= r_high {
+            partial_overlaps += 1;
+        } 
 
     }
 
-    println!("Full Overlaps: {}", full_overlaps);
+    let total_overlaps: u32 = full_overlaps + partial_overlaps; 
 
+    println!("Full Overlaps: {}", full_overlaps);
+    println!("Partial Overlaps: {}", partial_overlaps);
+    println!("Total Overlaps: {}", total_overlaps);
 }
 
 
